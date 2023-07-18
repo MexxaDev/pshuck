e3c6f71bb62664ba12e6ff5356a6c8f4f790fadd
Nahuel Beeguer <44383860+MexxaDev@users.noreply.github.com>
2023-07-18 19:28:15 -0300

    Add files via upload

---
+++ script/nav.js
@@ -0,0 +1,14 @@
+var burgermenu = document.getElementById("burgermenu");
+var burgerIcon = document.getElementById("burgerIcon");
+var burgerClosed = document.getElementById("burgerXIcon");
+
+function abrir (){
+    burgermenu.style.height = "200px";
+    burgerIcon.style.display = "none";
+    burgerClosed.style.display = "block";
+}
+function cerrar (){
+    burgermenu.style.height = "50px";
+    burgerIcon.style.display = "block";
+    burgerClosed.style.display = "none";
+}
